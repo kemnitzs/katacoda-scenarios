5d81036f4db1808208ae6da7b895f0db15c84cb9
Luis Martinez de Bartolome <lasote@gmail.com>
2019-04-08 12:24:16 +0200

    cat

--- katacodecomconanconanconsumer/courseBase.sh
+++ katacodecomconanconanconsumer/courseBase.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get -y install cmake
 pip install conan pygments
-mv /usr/local/show /usr/local/cat
+mv /usr/local/bin/show /bin/cat
 
 echo "done" >> /katacoda-background-finished
