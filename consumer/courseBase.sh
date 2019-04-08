57c0dbebc0a1a517e146a23ccbf8207830b24501
Luis Martinez de Bartolome <lasote@gmail.com>
2019-04-08 15:17:58 +0200

    cat

--- consumer/courseBase.sh
+++ consumer/courseBase.sh
@@ -3,5 +3,7 @@
 apt-get update
 apt-get -y install cmake
 pip install conan pygments
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
 
 echo "done" >> /katacoda-background-finished
